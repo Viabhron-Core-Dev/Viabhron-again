d185e43305e45fcafa96f6b4a242dde2c006ca57
Schuyler Lewis <schuylervion@proton.me>
2026-04-26 22:36:24 +0530

    ci: Temporarily disable NDK/CMake build for Android
    Comments out NDK and CMake configurations in the CI workflow and app build file. This change is intended to be temporary to address issues related to the JNI session. The `OfflineAiPlugin` registration in `MainActivity.java` is also commented out.

--- android/app/src/main/java/com/viabhron/mvk/MainActivity.java
+++ android/app/src/main/java/com/viabhron/mvk/MainActivity.java
@@ -2,12 +2,12 @@ package com.viabhron.mvk;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.vaa.plugins.offlineai.OfflineAiPlugin;
+// import com.vaa.plugins.offlineai.OfflineAiPlugin; // SIDELINED
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(OfflineAiPlugin.class);
+        // registerPlugin(OfflineAiPlugin.class); // SIDELINED
         super.onCreate(savedInstanceState);
     }
 }
